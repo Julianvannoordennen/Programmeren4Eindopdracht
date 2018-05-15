363a9e175f2c8670140e351998404ef717d8c880
Thomas De Lange <thomas.m.delange@gmail.com>
2018-05-16 00:08:41 +0200

    Develop versie 3.3
    Updated models

--- model/Maaltijd.js
+++ model/Maaltijd.js
@@ -1,15 +1,37 @@
 //Maaltijd class
+const ApiError = require("../model/ApiError");
+const assert = require('assert')
+
+
 class Maaltijd {
 
     //Constructor voor JSON Bericht gegevens
     constructor(naam, beschrijving, ingredienten, allergie, prijs){
-        this.naam = naam;
-        this.beschrijving = beschrijving;
-        this.ingredienten = ingredienten;
-        this.allergie = allergie;
-        this.prijs = prijs;
+
+      try {
+
+        //Controleer de JSON
+        assert(typeof naam === "string", "naam moet text zijn.")
+        assert(naam.trim().length > 2, 'naam must be at least 3 characters')
+        assert(typeof beschrijving === "string", "naam moet text zijn.")
+        assert(beschrijving.trim().length > 2, 'beschrijving must be at least 3 characters')
+        assert(typeof ingredienten === "string","ingredienten moet text  zijn.")
+        assert(ingredienten.trim().length > 2, 'ingredienten must be at least 3 characters')
+        assert(typeof allergie === "string", "allergie moet text zijn.")
+        assert(allergie.trim().length > 2, 'allergie must be at least 3 characters')
+        assert(typeof (prijs) === 'number', 'huisId must be a number.')
+        assert(!isNaN(prijs), 'huisId must be a number.')
+
+      } catch (ex) {
+          throw(new ApiError(ex.toString(), 422))
+      }
+
+        this.naam = naam.trim();
+        this.beschrijving = beschrijving.trim();
+        this.ingredienten = ingredienten.trim();
+        this.allergie = allergie.trim();
+        this.prijs = prijs.trim();
     }
 }
-
 //Exporteren voor gebruik bij andere files
 module.exports = Maaltijd;
--- model/UserRegisterJSON.js
+++ model/UserRegisterJSON.js
@@ -1,27 +1,56 @@
 const bcrypt = require('bcryptjs');
+const ApiError = require("../model/ApiError");
+const assert = require('assert')
+
+function validateEmail(email) {
+    const validator = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return validator.test(email);
+}
 
 //User register class
 class UserRegisterJSON {
 
     //Constructor voor JSON Bericht gegevens
     constructor(firstname, lastname, email, password){
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        
-        //Password encrypten
-        this.password = bcrypt.hashSync(password.trim(), 8);
-        /*bcrypt.hash(password.trim(), 8, (err, hash) => {
 
-            //Fout
-            if(err) 
-                throw(new ApiError(err.toString(), 500))
+      // Verify that we only create valid persons
+      try {
+          assert(typeof (firstname) === 'string', 'firstname must be a string')
+          assert(typeof (lastname) === 'string', 'lastname must be a string')
+          assert(typeof (email) === 'string', 'email must be a string')
+          assert(typeof (password) === 'string', 'password must be a string')
+          assert(firstname.trim().length > 2, 'firstname must be at least 3 characters')
+          assert(lastname.trim().length > 2, 'lastname must be at least 3 characters')
+          assert(validateEmail(email.trim()), 'email must be a valid emailaddress')
+          assert(password.trim().length > 2, 'password must be at least 3 characters')
+      }
+      catch (ex) {
+          throw(new ApiError(ex.toString(), 422))
+      }
 
-            //Correct, geencrypt wachtwoord opslaan
-            if(hash) 
-                this.password = hash
-        })*/
+      this.firstname = firstname.trim(),  // trim removes whitespace in front and at end
+      this.lastname = lastname.trim()
+      this.email = email.trim()
+
+      //Password encrypten
+      this.password = bcrypt.hashSync(password.trim(), 8);
+      /*bcrypt.hash(password.trim(), 8, (err, hash) => {
+
+          //Fout
+          if(err)
+              throw(new ApiError(err.toString(), 500))
+
+          //Correct, geencrypt wachtwoord opslaan
+          if(hash)
+              this.password = hash
+      })*/
     }
+}
+
+UserRegisterJSON.prototype.toString = function personToString() {
+    var copy = Object.assign({}, this);
+    delete copy.password
+    return copy
 }
 
 //Exporteren voor gebruik bij andere files

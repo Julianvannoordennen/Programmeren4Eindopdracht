9ff79c7704a863c23475d9d8f4bdcb25570fd066
Thomas De Lange <thomas.m.delange@gmail.com>
2018-05-16 09:24:31 +0200

    Develop versie 2.5
    Updated models

--- model/Maaltijd.js
+++ model/Maaltijd.js
@@ -1,13 +1,36 @@
 //Maaltijd class
+
+const ApiError = require("../model/ApiError");
+const assert = require('assert')
+
 class Maaltijd {
 
     //Constructor voor JSON Bericht gegevens
     constructor(naam, beschrijving, ingredienten, allergie, prijs){
-        this.naam = naam;
-        this.beschrijving = beschrijving;
-        this.ingredienten = ingredienten;
-        this.allergie = allergie;
-        this.prijs = prijs;
+
+      try {
+
+      //Controleer de JSON
+      assert(typeof naam === "string", "naam moet text zijn.")
+      assert(naam.trim().length > 2, 'naam must be at least 3 characters')
+      assert(typeof beschrijving === "string", "naam moet text zijn.")
+      assert(beschrijving.trim().length > 2, 'beschrijving must be at least 3 characters')
+      assert(typeof ingredienten === "string","ingredienten moet text  zijn.")
+      assert(ingredienten.trim().length > 2, 'ingredienten must be at least 3 characters')
+      assert(typeof allergie === "string", "allergie moet text zijn.")
+      assert(allergie.trim().length > 2, 'allergie must be at least 3 characters')
+      assert(typeof (prijs) === 'number', 'huisId must be a number.')
+      assert(!isNaN(prijs), 'huisId must be a number.')
+
+    } catch (ex) {
+        throw(new ApiError(ex.toString(), 412))
+    }
+
+      this.naam = naam;
+      this.beschrijving = beschrijving;
+      this.ingredienten = ingredienten;
+      this.allergie = allergie;
+      this.prijs = prijs;
     }
 }
 
